7a1f787fb5a44f02a235c6363de0aeda38e2dadf
Matthew Flynn <matthewtf73@gmail.com>
2017-11-16 14:19:00 -0500

    worked on models

--- project/app.py
+++ project/app.py
@@ -1,5 +1,8 @@
+# imports from the Flask framework
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+# imports for database handling
+from models import Base, Track
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
@@ -7,8 +10,12 @@ db = SQLAlchemy(app)
 
 @app.before_first_request
 def setup():
-    
+    # set up the database
+    Base.metadata.drop_all(bind=db.engine)
+    Base.metadata.create_all(bind=db.engine)
+    db.session.add(Track("test", "Soundtrack", "", False))
+    db.session.commit()
 
-in __name__ == '__main__':
+if __name__ == '__main__':
     app.debug = True
     app.run('127.0.0.1', 5000)
--- project/models.py
+++ project/models.py
@@ -1,11 +1,25 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative_base import declarative_base
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class Tracks(Base):
+# an Object to represent a track that a user can listen to
+class Track(Base):
 
     __tablename__ = 'tracks'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(127))
+    # the name of the track
+    name = Column(String(127), unique=True)
+    # acceptable values: Classical, Soundtrack
+    genre = Column(String(10))
+    # the path to the file
+    path = Column(String(255))
+    # indicates if the whole track will play or if just a 30 second sample will
+    full = Column(Boolean)
+
+    def __init__(self, name=None, genre="Soundtrack", path=None, full=False):
+        self.name = name
+        self.genre = genre
+        self.path = path
+        self.full = full
