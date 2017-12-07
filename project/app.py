5a4914e3eca1731abe875ae52ffabdac4994fa1f
Matthew Flynn <matthewtf73@gmail.com>
2017-12-07 13:24:23 -0500

    Need Nicks help with relationships

--- project/app.py
+++ project/app.py
@@ -15,13 +15,12 @@ def setup():
     Base.metadata.create_all(bind=db.engine)
     project1 = db.session.add(Project(name='No Hero', title='Audio Director',
                                       commisioner='Myself', genre='Soundtrack'))
-    db.session.add(Track(name='Mystery', path=track_path('Mystery.mp3'),
-                         full=0, project=project1))
-    db.session.add(Track(name='Town 1', path=track_path('Human Towns.mp3'),
-                         full=0, project=project1))
-    db.session.add(Track(name='Battle', path=track_path('Random Encounter.mp3'),
-                         full=0, project=project1))
-    db.session.add()
+    db.session.add(Track(name='Mystery', path='Mystery.mp3', full=0,
+                         project=project1))
+    db.session.add(Track(name='Town 1', path='Human Towns.mp3', full=0,
+                         project=project1))
+    db.session.add(Track(name='Battle', path='Random Encounter.mp3', full=0,
+                         project=project1))
     db.session.commit()
 
 @app.route('/music')
@@ -30,9 +29,6 @@ def music():
     tracks = db.session.query(Track).all()
     return render_template('music.html', projects=projects, tracks=tracks)
 
-def track_path(trackname):
-    return url_for('static', filename='samples/' + trackname)
-
 if __name__ == '__main__':
     app.debug = True
-    app.run('127.0.0.1', 8000)
+    app.run('127.0.0.1', 5000)
